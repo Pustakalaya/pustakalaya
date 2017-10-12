efe51eabeb54bc5034a9812e927536fb8f3bc506
Manoj Gautam <openpy.manoj@gmail.com>
2017-10-12 17:26:45 +0545

    gulp configured

---
+++ src/static/base_assets/base.js
@@ -0,0 +1,19 @@
+var $ = require("jquery")
+// now ui-kit css file
+// bootstrap as a dependencies
+import './css/bootstrap.min.css'
+import './sass/now-ui-kit.scss'
+
+// js files that now ui-kit depends on
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
+
+
+
+
+
+
+// now-ui-kit javascript file from now-ui theme
+import './js/now-ui-kit'
