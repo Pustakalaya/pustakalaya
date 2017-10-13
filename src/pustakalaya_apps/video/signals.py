2a5d2376042a1fed7557c7174c323c3ca91a952e
Manoj Gautam <openpy.manoj@gmail.com>
2017-10-13 19:40:42 +0545

    db transaction: implmented

--- src/pustakalaya_apps/video/signals.py
+++ src/pustakalaya_apps/video/signals.py
@@ -3,16 +3,19 @@
 
 from django.db.models.signals import post_save, pre_delete, m2m_changed
 from django.dispatch import receiver
+from django.db import transaction
 
 from .models import Video
 
 
 @receiver(m2m_changed, sender=Video.keywords.through)
-#@receiver(post_save, sender=Video)
+@receiver(post_save, sender=Video)
+@transaction.atomic
 def index_or_update_video(sender, instance, **kwargs):
     """Update or create an instance to index server."""
     # TODO: use logging system
     print("Instance", sender, instance)
+
     instance.index()
 
 
@@ -22,5 +25,3 @@ def delete_video(sender, instance, **kwargs):
     # TODO: Use logging system
     print("Delete index object")
     instance.delete_index()
-
-#m2m_changed.connect(index_or_update_video, sender=index_or_update_video)
