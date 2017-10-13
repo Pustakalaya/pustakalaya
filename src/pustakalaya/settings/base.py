e41e8fb1e411a9d2b8b8bb3484495ff720c4d704
Manoj Gautam <openpy.manoj@gmail.com>
2017-10-13 10:20:44 +0545

    template_filter: pustakalaya_filter implemented

--- src/pustakalaya/settings/base.py
+++ src/pustakalaya/settings/base.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ne'
 
 TIME_ZONE = 'Asia/Kathmandu'
 
@@ -222,8 +222,6 @@ LANGUAGES = (
     ('ne', _('Nepali')),
 )
 
-# Default language
-LANGUAGE_CODE = 'ne'
 
 # Translation local path
 LOCALE_PATHS = (
