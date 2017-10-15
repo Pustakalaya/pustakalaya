c82aa825388c19ec40ca0f5cd0fe09552aba8156
Manoj Gautam <openpy.manoj@gmail.com>
2017-10-15 13:11:01 +0545

    logging: logger configured

--- src/pustakalaya/settings/base.py
+++ src/pustakalaya/settings/base.py
@@ -259,3 +259,65 @@ AUTHENTICATION_BACKENDS = (
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+## Django logging settings
+LOG_DIR = os.path.join(os.path.dirname(BASE_DIR), 'logs')
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         },
+#         'null': {
+#             'level':'DEBUG',
+#             'class':'django.utils.log.NullHandler',
+#         },
+#         'console':{
+#             'level':'DEBUG',
+#             'class':'logging.StreamHandler',
+#             'formatter': 'verbose'
+#         },
+#         'logfile': {
+#             'level':'DEBUG',
+#             'class':'logging.handlers.RotatingFileHandler',
+#             'filename': os.path.join(LOG_DIR, 'pustakalaya.log'),
+#             'maxBytes': 1024*1024*5, # 5MB
+#             'backupCount': 0,
+#             'formatter': 'verbose',
+#         },
+#     },
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(levelname)s|%(asctime)s|%(module)s|%(process)d|%(thread)d|%(message)s',
+#             'datefmt' : "%d/%b/%Y %H:%M:%S"
+#         },
+#         'simple': {
+#             'format': '%(levelname)s|%(message)s'
+#         },
+#     },
+#     'loggers': {
+#         'myapp.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         'myapp.tasks': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#         'myapp.management': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         'myapp.models': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     }
+# }
