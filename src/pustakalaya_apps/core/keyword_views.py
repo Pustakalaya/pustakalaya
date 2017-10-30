0aae2a06366299493ef1c57103094c09665abc06
Manoj Gautam <manoj.gautam@olenepal.org>
2017-10-30 15:51:55 +0545

    Sorting features for keyword added

--- src/pustakalaya_apps/core/keyword_views.py
+++ src/pustakalaya_apps/core/keyword_views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
+from pustakalaya_apps.core.utils import list_search_from_elastic
 
 
 def keyword_detail(request, keyword):
-    keyword = " ".join(keyword.split("-"))
-    client = Elasticsearch()
-    # TODO: explicitly define the index name
-    s = Search().using(client).query("match", keywords=keyword)
-    response = s.execute()
-    context = {}
+    search_string = " ".join(keyword.split("-"))
+    # client = Elasticsearch()
+    # # TODO: explicitly define the index name
+    # s = Search().using(client).query("match", keywords=keyword)
+    # response = s.execute()
+    search_field = "keywords"
+    search_value = search_string
+    kwargs = {search_field:search_value}
 
-    context["response"] = response
+    # Query to elastic search in keywords field having the value of search_value
+    # Return response object.
+
+    context = list_search_from_elastic(request, **kwargs)
+    #
+    # context["response"] = response
     context["keyword"] = keyword
 
     return render(request, "core/keyword_detail.html", context)
+
+
