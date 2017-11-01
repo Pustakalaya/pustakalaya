2b2143337bf58f540c16fdfa488fdae42263e885
Navin Bahadur Rana <nabinrana102@gmail.com>
2017-11-01 10:44:13 +0545

    updated author list page

--- src/pustakalaya_apps/core/author_views.py
+++ src/pustakalaya_apps/core/author_views.py
@@ -19,9 +19,13 @@ class AuthorDetail(DetailView):
 def author_list(request):
     # hold some data.
     letters = string.ascii_lowercase
-    nepali_letters = [""]
+
+    nepali_letters = ['अ', 'आ', 'इ', 'ई', 'उ', 'ऋ', 'ए','क','ख','ग' ,'घ' ,'ङ', 'च', 'छ', 'ज', 'झ', 'ञ','ट', 'ठ', 'ड', 'ढ', 'ण','त', 'थ', 'द', 'ध', 'न', 'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ल', 'व', 'श', 'स', 'ष', 'ह']
+
     authors = Biography.objects.all()
     context = {}
     context["letters"] = letters
     context["authors"] = authors
+    context["nepali_letter"] = nepali_letters
+
     return render(request, "core/author_list.html", context)
