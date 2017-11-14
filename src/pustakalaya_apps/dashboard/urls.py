f174cc278f7e3002cc2644053e440dcbf69325ee
Manoj Gautam <nix1947@users.noreply.github.com>
2017-11-14 15:25:19 +0545

    dashboard page enhanced with setting menu (#79)

--- src/pustakalaya_apps/dashboard/urls.py
+++ src/pustakalaya_apps/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
 
-    url(r'^$', views.dashboard, name="dashboard")
+    url(r'^$', views.dashboard, name="dashboard"),
+    url(r'^profile/$', views.profile, name="dashboard")
 ]
