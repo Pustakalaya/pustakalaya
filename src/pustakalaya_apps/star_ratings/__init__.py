cb163054e554097d00a98c1ce1f48e30804e67f3
Navin Bahadur Rana <nabinrana102@gmail.com>
2017-12-04 15:33:47 +0545

    Removed star rating app and refactored code as per your reviews and suggestions (#111)

--- src/pustakalaya_apps/star_ratings/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from .app_settings import Settings
-
-__version__ = '0.5.5'
-
-default_app_config = 'pustakalaya_apps.star_ratings.apps.StarRatingsAppConfig'
-app_settings = Settings()
