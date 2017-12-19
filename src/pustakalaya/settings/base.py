817ceb62ba5157babee1e3736eba44f06da9ddba
Manoj Gautam <nix1947@users.noreply.github.com>
2017-12-19 17:31:08 +0545

    Update base.py

--- src/pustakalaya/settings/base.py
+++ src/pustakalaya/settings/base.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'pustakalaya.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3.bak'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
