7762d0fac90a2e98fcb0a7cff562fe61495b009f
Bhoj Bahadur Karki <nishantkarki305@gmail.com>
2018-02-26 10:23:31 +0545

    Filter for comment to admin (#162)

--- src/pustakalaya_apps/review_system/admin.py
+++ src/pustakalaya_apps/review_system/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Review
 # Register your models here.
+
+
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ('content_id', 'user', 'post', 'created', 'published')
+
+    search_fields = ('user','post','published')
+    list_filter = ('published',)
+    # def get_queryset(self, request):
+    #     return self.model.objects.filter(published="False")
+
+
+admin.site.register(Review,ReviewAdmin)
